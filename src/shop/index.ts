cd84d7989516cf944dc4bef07b937cb1945a3f9c
Daniel Ortega <contact@daniel-ortega.mx>
2020-03-19 00:40:02 -0600

    [shop]: abstraction of each kind of logic

--- src/shop/index.ts
+++ src/shop/index.ts
@@ -21,73 +21,93 @@ class Node {
 
 export default class Shop {
   config: any = {};
+  shops: any = [];
 
   constructor(config: any) {
     Object.assign(this, {config});
   };
 
-  getTime() {
-    const { totalShops, fishTypes, centers, roads } = this.config;
-    const nodes = [];
+  getShops() {
+    const { totalShops, centers } = this.config;
 
-    console.log('totalShops: ', totalShops);
-    console.log('fishTypes: ', fishTypes);
-    console.log('centers: ', centers);
-    console.log('roads: ', roads);
+    return [...new Array(totalShops)].map((shop: any, index: number) => (
+      new Node(centers[index].reduce((acc: any, current: any) => (
+        acc + (2 ** (current- 1))
+      ), 0))
+    ));
+  }
 
-    for (let i = 0; i < totalShops; i++) { // make nodes
-        let fishes = centers[i].reduce((sum: any, elem: any) => {
-            return sum + (2 ** (elem - 1)); // convert fishes to bit mask
-        }, 0);
-        nodes.push(new Node(fishes));
-    }
+  fillShopChildren(nodes: any) {
+    const { roads } = this.config;
 
-    for (let i = 0; i < roads.length; i++) { // put children
-        const r = roads[i];
-        nodes[r[0] - 1].children.push({
-            child: nodes[r[1] - 1],
-            time: r[2]
-        });
-        nodes[r[1] - 1].children.push({
-            child: nodes[r[0] - 1],
-            time: r[2]
-        });
-    }
+    roads.forEach((road: any, index: number) => {
+      nodes[road[0] - 1].children.push({
+        child: nodes[road[1] - 1],
+        time: road[2]
+      });
+      nodes[road[1] - 1].children.push({
+        child: nodes[road[0] - 1],
+        time: road[2]
+      });
+    });
 
+    return nodes;
+  }
+
+  fillFastesTime(nodes: any) {
     const nodesToUpdate = new Set();
     nodes[0].addTime(0, 0);
     nodesToUpdate.add(nodes[0]);
+
     while (nodesToUpdate.size > 0) { // fill out fastest times
-        const node: any = Array.from(nodesToUpdate)[0];
-        nodesToUpdate.delete(node);
-        for (let i = 0; i < node.children.length; i++) {
-            const child = node.children[i];
-            let didUpdate = false;
-            node.fastestTimes.forEach((time: any, index: any) => {
-                if (child.child.addTime(index, time + child.time)) {
-                    didUpdate = true;
-                }
-            });
-            if (didUpdate) {
-                nodesToUpdate.add(child.child);
-            }
+      const node: any = Array.from(nodesToUpdate)[0];
+      nodesToUpdate.delete(node);
+
+      for (let i = 0; i < node.children.length; i++) {
+        const child = node.children[i];
+        let didUpdate = false;
+        node.fastestTimes.forEach((time: any, index: any) => {
+          if (child.child.addTime(index, time + child.time)) {
+            didUpdate = true;
+          }
+        });
+        if (didUpdate) {
+          nodesToUpdate.add(child.child);
         }
+      }
     }
 
+    return nodes;
+  }
+
+  getBetterTime(nodes: any) {
+    const { totalShops, fishTypes } = this.config;
     const times = nodes[totalShops - 1].fastestTimes;
     let fastestTime: any = undefined;
+
     for (let i = 1; i < times.length; i++) {
-        if (times[i] !== undefined && (fastestTime === undefined || times[i] < fastestTime)) {
-            for (let j = i; j < times.length; j++) {
-                if (times[j] !== undefined && ((i | j) === (2 ** fishTypes) - 1)) {
-                    const slowerTime = Math.max(times[i], times[j]);
-                    if (fastestTime === undefined || slowerTime < fastestTime) {
-                        fastestTime = slowerTime;
-                    }
-                }
+      if (times[i] !== undefined && (fastestTime === undefined || times[i] < fastestTime)) {
+        for (let j = i; j < times.length; j++) {
+          if (times[j] !== undefined && ((i | j) === (2 ** fishTypes) - 1)) {
+            const slowerTime = Math.max(times[i], times[j]);
+            if (fastestTime === undefined || slowerTime < fastestTime) {
+              fastestTime = slowerTime;
             }
+          }
         }
+      }
     }
+
     return fastestTime;
+  }
+
+  getTime() {
+    const { totalShops, fishTypes, centers, roads } = this.config;
+
+    let nodes = this.getShops();
+    nodes = this.fillShopChildren(nodes);
+    nodes = this.fillFastesTime(nodes);
+
+    return this.getBetterTime(nodes);
   }
 }
