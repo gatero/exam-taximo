48355d1f6744e035e0bd1f3cb6b06e034fab9fcc
Daniel Ortega <contact@daniel-ortega.mx>
2020-03-18 23:30:00 -0600

    [meet]: change the way to parse paramas

--- src/controllers/meet.controller.ts
+++ src/controllers/meet.controller.ts
@@ -20,10 +20,8 @@ const MEET_RESPONSE: ResponseObject = {
 /**
  * A simple controller to bounce back http requests
  */
-export class MeetController {
-  constructor(
-    @inject(RestBindings.Http.REQUEST) private req: Request
-  ) {}
+export class meetController {
+  constructor(@inject(RestBindings.Http.REQUEST) private req: Request) {}
 
   // Map to `GET /meet`
   @get('/meet', {
@@ -42,20 +40,13 @@ export class MeetController {
     const shopConfig = {
       totalShops: this.getTotalShops(parameters),
       fishTypes: this.getFishTypes(parameters),
-      centers: this.parseStringToArray(shopping_centers),
-      roads: this.parseStringToArray(roads),
+      centers: this.getCenters(shopping_centers),
+      roads: this.getRoads(roads),
     };
 
     const shop = new Shop(shopConfig);
-    shop.getTime();
 
-    return 30;
-  }
-
-  getFishTypes(parameters: string): number {
-    const [,,fishTypes] = parameters.split(',');
-
-    return parseInt(fishTypes);
+    return shop.getTime();
   }
 
   getTotalShops(parameters: string): number {
@@ -64,9 +55,32 @@ export class MeetController {
     return parseInt(totalShops);
   }
 
-  parseStringToArray(input: string) {
-    return input.split('-').map((item: any) => (
-      item.split(',').map((value: any) => parseInt(value))
-    ));
+  getCenters(shoping_centers: string): any {
+    const parsedCenters = shoping_centers.split('-').map((center: any) => {
+      center = center.split(',').slice(1);
+      center = center.map((value: any) => parseInt(value));
+
+      return center;
+  });
+
+    return parsedCenters;
+  }
+
+  getFishTypes(parameters: string): number {
+    const [,,fishTypes] = parameters.split(',');
+
+    return parseInt(fishTypes);
+  }
+
+  getRoads(roads: string): any {
+    const parsedRoads = roads.split('-').map((road: any) => {
+      road = road.split(',');
+      road = road.map((value: any) => parseInt(value));
+
+      return road;
+    });
+
+    return parsedRoads;
   }
 }
+
