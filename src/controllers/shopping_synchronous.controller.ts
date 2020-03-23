aa828b1f86cee160397ec9d14ba2a9beeba74311
Daniel Ortega <contact@daniel-ortega.mx>
2020-03-22 23:44:25 -0600

    [docs]: add documentation for the shopping_synchronous.controller

--- src/controllers/shopping_synchronous.controller.ts
+++ src/controllers/shopping_synchronous.controller.ts
@@ -19,15 +19,19 @@ import {
   SHOPPING_SYNCHRONOUS_RESPONSE, 
 } from '../shopping-synchronous';
 
-
 /**
- * A simple controller to bounce back http requests
+ * @class ShoppingSynchronousController
+ * @description A simple controller to bounce back http requests,
+ * this contains all the necessary methods to validate the request body.
  */
 export class ShoppingSynchronousController {
   validate: any = {};
   shopConfig: any = {};
   requestBody: any = {};
 
+  /**
+   * @constructs
+   * */
   constructor(
     @inject(RestBindings.Http.REQUEST) private req: Request,
     @repository(UserRepository) public userRepository: UserRepository,
@@ -36,7 +40,13 @@ export class ShoppingSynchronousController {
     this.validate = new ShoppingSynchronousValidate(userRepository, shopRepository);
   }
 
-  // Map to `GET /shopping_synchronous`
+  /** 
+   * @function shopping_synchronous
+   * @param {request} incoming data
+   * @description Map to `GET /shopping_synchronous`, this function
+   * contains the mixed logic for synchronous endpoint.
+   * @returns {response} An object which contains the minimum_time
+   * */
   @post('/shopping_synchronous', {
     responses: {
       '200': SHOPPING_SYNCHRONOUS_RESPONSE,
@@ -58,7 +68,7 @@ export class ShoppingSynchronousController {
 
       const shopExist = await this.validate.shop(this.requestBody);
 
-      const {isValidShopConfig, shopConfigValidationMessage} = this.getShopConfig(this.requestBody);
+      const {isValidShopConfig, shopConfigValidationMessage} = this.setShopConfig();
       if (!isValidShopConfig) {
         return shopConfigValidationMessage;
       }
@@ -81,15 +91,21 @@ export class ShoppingSynchronousController {
     }
   }
 
-  getShopConfig(body: any): any {
+  /**
+   * @function setShopConfig
+   * @description process the request body and set the configuration for request handling
+   * it always returns an object with the operation result.
+   * @returns {object}
+   * */
+  setShopConfig(): any {
     Object.assign(this.shopConfig, {
-      totalShops: this.getTotalShops(body.parameters),
-      fishTypes: this.getFishTypes(body.parameters),
-      centers: this.getCenters(body.shopping_centers),
-      roads: this.getRoads(body.roads),
+      totalShops: this.getTotalShops(this.requestBody.parameters),
+      fishTypes: this.getFishTypes(this.requestBody.parameters),
+      centers: this.getCenters(this.requestBody.shopping_centers),
+      roads: this.getRoads(this.requestBody.roads),
     });
     const {totalShops, centers, roads} = this.shopConfig;
-    const [,fishTypes] = body.parameters.split(',');
+    const [,fishTypes] = this.requestBody.parameters.split(',');
 
     if (totalShops !== centers.length) {
       return {
@@ -111,18 +127,39 @@ export class ShoppingSynchronousController {
     }
   }
 
+  /**
+   * @function getTotalShops
+   * @param {string} parameters
+   * @description takes the param called params and takes the first position
+   * which contains the total of shopping centers.
+   * @returns {number} totalShops
+   * */
   getTotalShops(parameters: string): number {
     const [totalShops] = parameters.split(',');
 
     return parseInt(totalShops);
   }
 
+  /**
+   * @function getFishTypes
+   * @param {string} parameters
+   * @description takes the param called params and takes the third position
+   * which contains the total of fishTypes.
+   * @returns {number} fishTypes
+   * */
   getFishTypes(parameters: string): number {
     const [,,fishTypes] = parameters.split(',');
 
     return parseInt(fishTypes);
   }
 
+  /**
+   * @function getCenters
+   * @param {string} shopping_centers
+   * @description split the shopping_centers of type string fist by '-' and
+   * then by ',' and then slicing the first element of each center.
+   * @returns {array} parsedCenters
+   * */
   getCenters(shoping_centers: string): any {
     const parsedCenters = shoping_centers.split('-').map((center: any) => {
       center = center.split(',').slice(1);
@@ -134,6 +171,12 @@ export class ShoppingSynchronousController {
     return parsedCenters;
   }
 
+  /**
+   * @function getRoads
+   * @param {string} roads
+   * @description split the roads string fist by '-' and then by ','.
+   * @returns {Array<Array<number>>} parsedCenters
+   * */
   getRoads(roads: string): any {
     const parsedRoads = roads.split('-').map((road: any) => {
       road = road.split(',');
